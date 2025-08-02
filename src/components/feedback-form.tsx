d45096bdba25081089c069ab2702aad02db15e33
Ogunbunmi <o0325095@gmail.com>
2025-08-02 13:26:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.label c

--- src/components/feedback-form.tsx
+++ src/components/feedback-form.tsx
@@ -147,7 +147,7 @@ export function FeedbackForm() {
                           className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary w-full cursor-pointer transition-colors"
                         >
                           <Store className="mb-3 h-6 w-6" />
-                          Brand/Exhibitor
+                          Brand
                         </Label>
                       </FormItem>
                     </RadioGroup>
